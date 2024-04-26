a14df82a7ba7afb4f6db4f3245ac9e8c164dab85
Bharath Kumar Reddy <bharathkumarreddy@Bharaths-MacBook-Air.local>
2024-04-26 19:04:41 +0530

    first commit

---
+++ Arrays-Lists/one.py
@@ -0,0 +1,9 @@
+from array import *
+input_array = array('i', [11,234,12,1,10,2,21])
+sorted(input_array)
+
+print("you're sorted array is: ")
+for i in range(len(input_array)):
+    
+    print(input_array[i], end = ' ')
+    
