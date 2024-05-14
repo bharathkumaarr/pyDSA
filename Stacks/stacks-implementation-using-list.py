d569bd38d537ea474f1e127b4101c3cae7dd7132
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2024-05-14 10:36:10 +0530

    stacks implementation using Lists added

---
+++ Stacks/stacks-implementation-using-list.py
@@ -0,0 +1,62 @@
+'''
+push ----> stack.append(x)
+pop ----> stack.pop()
+peek/top ----> stack.peek()
+isEmpty
+'''
+
+#stack Implementation using Lists
+
+stack = []
+
+def append_to_stack():
+    x=input("enter the element to append/push to stack: ")
+    stack.append(x)
+    print("your new stack is: ", stack)
+
+def remove_from_stack():
+    if not(not stack):
+        stack.pop()
+        print("your new stack is: ", stack)
+    else:
+        print("stack empty")
+    
+def peek():
+    if not stack:
+        print("stack empty")
+    else:
+        print("top element of your stack is: ", stack[-1])
+
+def isEmpty():
+    if not stack:
+        print("stack is empty")
+    else:
+        print("stack is NOT empty")
+
+        
+while True:
+    print('''
+enter your choice:
+1.Push
+2.Pop
+3.Peek
+4.check if empty
+5.Display Stack
+6.Quit''')
+    ch = int(input())
+    if ch == 1:
+        append_to_stack()
+    elif ch == 2:
+        remove_from_stack()
+    elif ch == 3:
+        peek()
+    elif ch == 4:
+        isEmpty()
+    elif ch == 5:
+        print("your stack is: ", stack)
+    elif ch == 6:
+        break
+    else:
+        print("invalid choice")
+    
+        
